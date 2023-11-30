ca57c13613a25756f6a9847edec0c9e43ca5e65e
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-11-30 04:50:51 +0200

    100-puppet_ssh_config.pp

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# using puppet
+
+file { 'etc/ssh/ssh_config':
+	ensure => present,
+
+content =>"
+	#ssh config
+	host *
+	IdentityFile ~/.ssh/school
+	PasswordAuthentication no
+}
