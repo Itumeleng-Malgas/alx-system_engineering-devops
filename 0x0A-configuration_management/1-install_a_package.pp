78e5352b33599c140c6a81f662a0f47314a1eb43
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-12-22 14:46:56 +0200

    flask not capitalized

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,10 @@
-#!/usr/bin/pup
 # Filename: 1-install_a_package.pp, install flask from pip3
 
 package { 'python3-pip':
   ensure => installed,
 }
 
-package { 'Flask':
+package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
   require  => Package['python3-pip'],
