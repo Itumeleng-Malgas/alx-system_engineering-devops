e57c7ae823d4675a0b175d5311375168c5dfa279
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-12-22 16:28:51 +0200

    Waiting for the checker

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,11 @@
-#!/usr/bin/env bash
 # using puppet
 
 file { 'etc/ssh/ssh_config':
-	ensure => present,
-
-content =>"
-	#ssh config
-	host *
-	IdentityFile ~/.ssh/school
-	PasswordAuthentication no
-	",
+  ensure  => present,
+  content => '
+    #ssh config
+    host *
+    IdentityFile ~/.ssh/school
+    PasswordAuthentication no
+  '
 }
