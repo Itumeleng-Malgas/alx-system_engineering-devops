a085c8b47f504b26cb06a8208b4dad4faf7274ed
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-12-22 14:29:05 +0200

    Tasl 1: Require python3-pip

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 # Filename: 1-install_a_package.pp, install flask from pip3
 
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  unless  => '/usr/bin/pip3 show Flask | grep -q "^Version: 2.1.0$"',
+package { 'python3-pip':
+  ensure => installed,
+}
+
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }
