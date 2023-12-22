a7645e704f10ab012d0cca1fe9177d2872de6a98
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-12-22 15:22:27 +0200

    manually installing all packages

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,26 @@
-# Install Flask version 2.1.0 using pip3
+# Installs flask and required packages
 
-package { 'python3-pip':
-  ensure => installed,
+# Install Python 3.8.10
+package { 'python3.8':
+  ensure => '3.8.10',
 }
 
+# Install pip for Python package management
+package { 'python3-pip':
+  ensure => present,
+}
+
+# Install Flask 2.1.0
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip',
   require  => Package['python3-pip'],
 }
+
+# Install Werkzeug 2.1.1
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip',
+  require  => Package['python3-pip'],
+}
+
