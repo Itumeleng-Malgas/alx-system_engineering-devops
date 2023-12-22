f28342c2d0b58250fd8e122031ef0b2c9d4d88de
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-12-22 12:10:21 +0200

    0x0A-configuration_management

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Filename: 0-create_a_file.pp, create a file in /tmp.
+
+file { '/tmp/school':
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
