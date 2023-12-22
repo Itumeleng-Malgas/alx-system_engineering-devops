434f4b3acfd80e8a350492bb7b9c4e53617b989a
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-12-22 14:20:15 +0200

    Making use of exec

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,8 @@
-# Filename: 1-install_a_package.pp
+# Filename: install_flask.pp
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+# Install Flask
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => ['/usr/bin'],
+  unless  => '/usr/bin/pip3 show Flask | grep -q "^Version: 2.1.0$"',
 }
