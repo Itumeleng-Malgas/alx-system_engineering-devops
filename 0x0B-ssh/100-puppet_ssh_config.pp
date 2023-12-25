d01d7855fa960efe5249d314b42b86a7d48ea6b7
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-12-25 10:54:55 +0200

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,12 @@
-# using puppet
+# set up your client SSH configuration file so that you can
+# connect to a server without typing a password.
 
 file { 'etc/ssh/ssh_config':
   ensure  => present,
-  content => '
+  content => "
     #ssh config
     host *
     IdentityFile ~/.ssh/school
     PasswordAuthentication no
-  '
+  "
 }
