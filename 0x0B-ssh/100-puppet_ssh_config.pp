ad920f6003d09a27cbe020b672d2e49cb9d475f4
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-12-25 11:00:59 +0200

    100-puppet_ssh_config.pp: changed path param

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,7 @@
 # set up your client SSH configuration file so that you can
 # connect to a server without typing a password.
 
-file { 'etc/ssh/ssh_config':
+file { '/etc/ssh/ssh_config':
   ensure  => present,
   content => "
     #ssh config
