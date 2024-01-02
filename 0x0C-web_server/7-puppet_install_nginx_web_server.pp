c047b66f60132653916bbb2515691725e9c67415
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-01-02 18:02:16 +0200

    1-install_nginx_web_server

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,39 @@
+# Install Nginx package
+package { 'nginx':
+  ensure => 'installed',
+}
+
+# Ensure Nginx service is running and enabled
+service { 'nginx':
+  ensure  => 'running',
+  enable  => true,
+  require => Package['nginx'],
+}
+
+# Define the default site configuration
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  content => "server {
+    listen 80;
+    server_name 3.85.148.189;
+
+    location / {
+        echo 'Hello World!';
+    }
+
+    location /redirect_me {
+        return 301 https://example.com/;
+    }
+}\n",
+  notify  => Service['nginx'],
+}
+
+# Reload Nginx after making changes
+exec { 'reload_nginx':
+  command     => '/bin/kill -s HUP $(cat /var/run/nginx.pid)',
+  refreshonly => true,
+  subscribe   => File['/etc/nginx/sites-available/default'],
+}
