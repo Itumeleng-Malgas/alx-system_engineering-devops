f835f23302349fd0dd934164addcca1dd7de724f
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-01-29 19:32:16 +0200

    1-export_to_CSV.py

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+"""Script that uses the requests module"""
+
+import requests
+import sys
+
+
+def fetch_employee_data(employee_id):
+    """
+    Fetches employee data from the JSONPlaceholder API.
+
+    Parameters:
+        employee_id (int): The ID of the employee.
+
+    Returns:
+        dict: Employee data.
+    """
+    url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    response = requests.get(url)
+    employee_data = response.json()
+    return employee_data
+
+
+def fetch_todo_list(employee_id):
+    """
+    Fetches the list of todos for a specific employee from the
+    JSONPlaceholder API.
+
+    Parameters:
+        employee_id (int): The ID of the employee.
+
+    Returns:
+        list: List of todos.
+    """
+    url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    response = requests.get(url)
+    todo_list = response.json()
+    return todo_list
+
+
+def display_todo_progress(employee_id):
+    """
+    Displays the progress of todos for a specific employee.
+
+    Parameters:
+        employee_id (int): The ID of the employee.
+    """
+    employee_data = fetch_employee_data(employee_id)
+    todo_list = fetch_todo_list(employee_id)
+
+    employee_name = employee_data.get('name')
+    completed_tasks = [task for task in todo_list if task['completed']]
+
+    total_tasks = len(todo_list)
+    completed_task_count = len(completed_tasks)
+
+    print("Employee {} is done with tasks ({}/{}):".format(
+        employee_name, completed_task_count, total_tasks))
+
+    for task in completed_tasks:
+        print(f"\t {task['title']}")
+
+
+if __name__ == "__main__":
+    employee_id = sys.argv[1]
+
+    try:
+        employee_id = int(employee_id)
+    except ValueError:
+        print("Employee ID must be an integer.")
+        sys.exit(1)
+
+    display_todo_progress(employee_id)
