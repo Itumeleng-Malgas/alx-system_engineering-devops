74a799e0636897756f4b1c6fe460a5c2d80c6294
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-02-13 12:12:51 +0200

    0x16-api_advanced

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+"""
+Module that that queries the Reddit API and returns the
+number of subscribers
+"""
+import requests
+
+def number_of_subscribers(subreddit):
+    """ Get the number of Reddit subscribers """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "MyBot/1.0"}
+    
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
+        return 0
