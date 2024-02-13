d7953d0d1e491f56be0294e95e207ecbcf53d849
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-02-13 17:58:27 +0200

    New user agent

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,9 @@ import requests
 def number_of_subscribers(subreddit):
     """ Get the number of Reddit subscribers """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "MyBot/1.0"}
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+               "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45"
+               "Safari/537.36"}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
 
