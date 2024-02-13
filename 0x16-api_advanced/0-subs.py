f88bfb20a1705d484449f189b535751741793943
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-02-13 22:44:18 +0200

    pycodestyle fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,7 @@ def number_of_subscribers(subreddit):
     """ Get the number of Reddit subscribers """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'Custom-User-Agent'}
-    
+
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
