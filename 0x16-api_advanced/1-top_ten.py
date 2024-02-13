957d529bfe6244e9f4938a58cb9570d3cd10c0b0
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-02-13 12:49:40 +0200

    1-top_ten.py: first commit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Module that queries the Reddit API and prints the titles of the first 10
+hot posts listed for a given subreddit.
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """ Function that queries the Reddit API """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {'User-Agent': 'Custom-User-Agent'}
+
+    try:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            for post in response.json()['data']['children']:
+                print(post['data']['title'])
+        elif response.status_code == 404:
+            print(None)
+    except Exception as e:
+        print(None)
