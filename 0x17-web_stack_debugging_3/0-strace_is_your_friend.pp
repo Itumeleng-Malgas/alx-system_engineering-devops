581b114a6a2c5cdbfe87a782971c8010f48a65e7
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-02-15 04:04:11 +0200

    0-strace_is_your_friend.pp: works1

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # fix wp-settings.phpp file extention.
 
-{ 'fix-bad-extension':
+exec { 'fix-bad-extension':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/',
 }
