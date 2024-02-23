442882b832557b7325682565bf6aa2b8cb913cde
Itumeleng Malgas <aaron.tumi@live.co.za>
2024-02-23 05:07:45 +0200

    1-user_limit.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,13 @@
 #  Sky is the limit, let's bring that limit higher
 
 # Increase ULIMIT
-exec {
+exec { 'increase-limit':
     command => "/bin/sed -i 's/15/4096/' /etc/default/nginx",
     path    => "/usr/local/bin/:/bin/",
 }
 
 # Restart nginx
-exec {
+exec { 'restart-nginx':
     command => "/etc/init.d/nginx restart",
     path    => "/etc/init.d",
 }
